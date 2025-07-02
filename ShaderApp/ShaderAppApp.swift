020558b65dafa12e5cb6e2a9c42122740c3badf6
gunnargray <hi@gunnargray.com>
2025-07-01 21:16:19 -0500

    Initial Commit

---
+++ ShaderApp/ShaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderAppApp.swift
+//  ShaderApp
+//
+//  Created by Gunnar Gray on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ShaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
